53b620d9258cf395a7ef4f898547d92633193771
clover <2621869236@qq.com>
2023-09-03 14:33:18 +0800

    feat: 添加单元测试依赖

---
+++ wisdom-common/src/main/java/top/ctong/wisdom/common/model/dto/product/unit/UnitListResponse.java
@@ -0,0 +1,87 @@
+package top.ctong.wisdom.common.model.dto.product.unit;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * █████▒█      ██  ▄████▄   ██ ▄█▀     ██████╗ ██╗   ██╗ ██████╗
+ * ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒      ██╔══██╗██║   ██║██╔════╝
+ * ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░      ██████╔╝██║   ██║██║  ███╗
+ * ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄      ██╔══██╗██║   ██║██║   ██║
+ * ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄     ██████╔╝╚██████╔╝╚██████╔╝
+ * ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒     ╚═════╝  ╚═════╝  ╚═════╝
+ * ░     ░░▒░ ░ ░   ░  ▒   ░ ░▒ ▒░
+ * ░ ░    ░░░ ░ ░ ░        ░ ░░ ░
+ * ░     ░ ░      ░  ░
+ * Copyright 2023 Clover You.
+ * <p>
+ * 查询所有单位
+ * </p>
+ *
+ * @author Clover
+ * @date 2023-07-26 16:27
+ */
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "查询所有单位")
+public class UnitListResponse implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 7716678547495664955L;
+
+    /**
+     * 单位id
+     */
+    @TableId(type = IdType.ASSIGN_ID)
+    @Schema(description = "单位id")
+    private Long unitId;
+
+    /**
+     * 单位名称
+     */
+    @Schema(description = "单位名称")
+    private String unitName;
+
+    /**
+     * 备注
+     */
+    @Schema(description = "备注")
+    private String unitRemark;
+
+    /**
+     * 启用状态 0=禁用；1=启用
+     */
+    @Schema(description = "启用状态 0=禁用；1=启用")
+    private Short enable;
+
+    /**
+     * 是否允许小数 0=不支持；1=支持
+     */
+    @Schema(description = "是否允许小数 0=不支持；1=支持")
+    private Short isDecimal;
+
+    /**
+     * 排序
+     */
+    @Schema(description = "排序")
+    private Integer sort;
+
+    /**
+     * 创建时间
+     */
+    @Schema(description = "创建时间")
+    @JsonFormat(pattern = "yyyy-MM-dd hh:mm ss")
+    private Date createAt;
+}
